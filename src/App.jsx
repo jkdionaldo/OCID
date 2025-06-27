9ba50848a8a7dd479d7eb33cfa78ea5f44cd96af
shinjitsue <jorenverdad84@gmail.com>
2025-06-27 10:10:11 +0800

    refactor: restructure college, undergrad, and graduate program pages into modular components
    - Moved program data and logic into per-college files for better maintainability
    - Updated imports and structure for clarity
    - No changes to business logic or features

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
-"use client"
+"use client";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
 
-import Navbar from "./assets/components/Navbar"
-import Footer from "./assets/components/Footer"
-import Home from "./assets/pages/Home"
-import Colleges from "./assets/pages/Colleges"
-import CCISGraduate from "./assets/pages/CCIS-Graduate"
-import CAAGraduate from "./assets/pages/CAA-Graduate"
-import CEDGraduate from "./assets/pages/CED-Graduate"
-import CMNSGraduate from "./assets/pages/CMNS-Graduate"
-import COFESGraduate from "./assets/pages/COFES-Graduate"
-import CHASSGraduate from "./assets/pages/CHASS-Graduate"
-import Undergrad from "./assets/pages/Undergrad"
-import CCISUndergrad from "./assets/pages/CCIS-Undergrad"
-import CEDUndergrad from "./assets/pages/CED-Undergrad"
-import CAAUndergrad from "./assets/pages/CAA-Undergrad"
-import CMNSUndergrad from "./assets/pages/CMNS-Undergrad"
-import CEGSUndergrad from "./assets/pages/CEGS-Undergrad"
-import CHASSUndergrad from "./assets/pages/CHASS-Undergrad"
-import COFESUndergrad from "./assets/pages/COFES-Undergrad"
-import Navigate from "./assets/components/Navigate"
-import "./App.css"
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Home from "@/pages/Home";
+import Colleges from "@/pages/Colleges";
+import CCISGraduate from "@/pages/CCIS-Graduate";
+import CAAGraduate from "@/pages/CAA-Graduate";
+import CEDGraduate from "@/pages/CED-Graduate";
+import CMNSGraduate from "@/pages/CMNS-Graduate";
+import COFESGraduate from "@/pages/COFES-Graduate";
+import CHASSGraduate from "@/pages/CHASS-Graduate";
+import Undergrad from "@/pages/Undergrad";
+import CCISUndergrad from "@/pages/CCIS-Undergrad";
+import CEDUndergrad from "@/pages/CED-Undergrad";
+import CAAUndergrad from "@/pages/CAA-Undergrad";
+import CMNSUndergrad from "@/pages/CMNS-Undergrad";
+import CEGSUndergrad from "@/pages/CEGS-Undergrad";
+import CHASSUndergrad from "@/pages/CHASS-Undergrad";
+import COFESUndergrad from "@/pages/COFES-Undergrad";
+import Navigate from "@/components/Navigate";
+import "./App.css";
 
 // Import the Google OAuth provider
-import { GoogleOAuthProvider } from "@react-oauth/google"
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // Simple redirect component
 const CollegesRedirect = () => {
-  return <Navigate to="/graduate" replace={true} />
-}
+  return <Navigate to="/graduate" replace={true} />;
+};
 
 function App() {
   // Your actual Google Client ID
-  const googleClientId = "823708007386-k6iatijd2d2p9trobjsavg85batkn5th.apps.googleusercontent.com"
+  const googleClientId =
+    "823708007386-k6iatijd2d2p9trobjsavg85batkn5th.apps.googleusercontent.com";
 
   return (
     <GoogleOAuthProvider clientId={googleClientId}>
@@ -46,28 +46,50 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
-              
-             
 
               {/* Graduate College Routes */}
               <Route path="colleges/ccis-graduate" element={<CCISGraduate />} />
               <Route path="colleges/caa-graduate" element={<CAAGraduate />} />
               <Route path="colleges/ced-graduate" element={<CEDGraduate />} />
               <Route path="colleges/cmns-graduate" element={<CMNSGraduate />} />
-              <Route path="colleges/cofes-graduate" element={<COFESGraduate />} />
-              <Route path="colleges/chass-graduate" element={<CHASSGraduate />} />
-             
-             
-            
+              <Route
+                path="colleges/cofes-graduate"
+                element={<COFESGraduate />}
+              />
+              <Route
+                path="colleges/chass-graduate"
+                element={<CHASSGraduate />}
+              />
 
-               {/* Undergraduate College Routes */}
-               <Route path="/undergrad/ccis-undergrad" element={<CCISUndergrad />} />
-              <Route path="/undergrad/caa-undergrad" element={<CAAUndergrad />} />
-              <Route path="/undergrad/ced-undergrad" element={<CEDUndergrad />} />
-              <Route path="/undergrad/cofes-undergrad" element={<COFESUndergrad />} />
-              <Route path="/undergrad/chass-undergrad" element={<CHASSUndergrad />} />
-              <Route path="/undergrad/cegs-undergrad" element={<CEGSUndergrad />} />
-              <Route path="/undergrad/cmns-undergrad" element={<CMNSUndergrad />} />
+              {/* Undergraduate College Routes */}
+              <Route
+                path="/undergrad/ccis-undergrad"
+                element={<CCISUndergrad />}
+              />
+              <Route
+                path="/undergrad/caa-undergrad"
+                element={<CAAUndergrad />}
+              />
+              <Route
+                path="/undergrad/ced-undergrad"
+                element={<CEDUndergrad />}
+              />
+              <Route
+                path="/undergrad/cofes-undergrad"
+                element={<COFESUndergrad />}
+              />
+              <Route
+                path="/undergrad/chass-undergrad"
+                element={<CHASSUndergrad />}
+              />
+              <Route
+                path="/undergrad/cegs-undergrad"
+                element={<CEGSUndergrad />}
+              />
+              <Route
+                path="/undergrad/cmns-undergrad"
+                element={<CMNSUndergrad />}
+              />
               {/* Redirect for any other paths */}
               <Route path="*" element={<Navigate to="/" replace />} />
 
@@ -80,9 +102,7 @@ function App() {
         </div>
       </Router>
     </GoogleOAuthProvider>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
