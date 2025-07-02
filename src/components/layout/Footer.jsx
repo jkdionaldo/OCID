683a62bf13f2e892a8493484fd886ab39a47cedb
cjbantillo <bantillocj1@example.com>
2025-07-02 09:07:28 +0800

    fix: reduce padding in Footer component for improved layout

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <footer className="bg-gray-800 text-white py-6">
+      <footer className="bg-gray-800 text-white py-2">
         <div className="container mx-auto px-6">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-center md:text-left mb-4 md:mb-0">
