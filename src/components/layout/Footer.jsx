abdb3db375bc71595ea28f6af19dc0ae58cd176c
kev <jankevin.dionaldo64@gmail.com>
2025-07-15 14:45:33 +0800

    misc: adjusted the first part (texts)  of footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,6 +2,16 @@ const Footer = () => {
     return (
       <footer className="bg-gray-900 text-white py-2 text-sm">
         <div className="container mx-auto px-6">
+          {/*For part 1 footer*/}
+          <div className="w-full md:pb-5">
+            <p>For concerns and Inquiry about the website please visit<br></br><a
+              href="https://ocid.carsu.edu.ph"
+              className="text-[#2356f6] underline"
+              target="_blank"
+            >
+              Office of Curriculum and Instruction Development (OCID)
+            </a><br></br>or email at ocid@carsu.edu.ph</p>
+          </div>
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-center md:text-left mb-4 md:mb-0">
               Copyright &copy; Caraga State University {new Date().getFullYear()}
