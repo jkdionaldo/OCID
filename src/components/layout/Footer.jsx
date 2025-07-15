56567a98d8df54ee92616ffdd3d96f1c01e659d5
kev <jankevin.dionaldo64@gmail.com>
2025-07-15 16:02:41 +0800

    misc: added a comment for footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -12,6 +12,7 @@ const Footer = () => {
               Office of Curriculum and Instruction Development (OCID)
             </a><br></br>or email at <a className="text-[#2356f6]">ocid@carsu.edu.ph</a></p>
           </div>
+             {/*For part 2 footer*/}
           <div className="flex flex-col md:flex-row items-center mx-16">
             <img src='/images/CSU-Official-Seal_1216-x-2009-1.svg'></img>
             <img src='/images/Hi-Res-BAGONG-PILIPINAS-LOGO-1474x1536-1-1.svg'></img>
