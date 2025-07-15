f59d5637506af0909c16eb4cb9ec754cce61158f
kev <jankevin.dionaldo64@gmail.com>
2025-07-15 16:14:09 +0800

    misc: added underline when footer links being hovered

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -20,13 +20,13 @@ const Footer = () => {
               Copyright &copy; Caraga State University {new Date().getFullYear()}
             </p>
             <div className="flex space-x-4 ml-auto md:mt-0">
-              <a href="#" className="text-gray-300 hover:text-white transition-colors duration-200">
+              <a href="#" className="text-white hover:underline transition-colors duration-200">
                 Privacy Policy
               </a>
-              <a href="#" className="text-gray-300 hover:text-white transition-colors duration-200">
+              <a href="#" className="text-white hover:underline transition-colors duration-200">
                 Terms of Service
               </a>
-              <a href="#" className="text-gray-300 hover:text-white transition-colors duration-200">
+              <a href="#" className="text-white hover:underline transition-colors duration-200">
                 Contact
               </a>
             </div>
