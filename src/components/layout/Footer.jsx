809573c69f429461b3df6bc33be366a6db00c5e0
kev <jankevin.dionaldo64@gmail.com>
2025-07-17 14:53:31 +0800

    refactor: fixed the responsiveness of the footer in the mobile view

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
       <footer className="bg-gray-900 text-white py-2 text-sm">
-        <div className="container mx-auto px-6">
+        <div className="container mx-auto md:px-16">
           {/*For part 1 footer*/}
-          <div className="w-full md:py-2 text-xs">
+          <div className="w-full text-center md:py-2 text-[9px] md:text-xs">
             <p>For concerns and Inquiry about the website please visit<br></br><a
               href="https://ocid.carsu.edu.ph"
               className="text-[#2356f6] underline"
@@ -12,18 +12,20 @@ const Footer = () => {
               Office of Curriculum and Instruction Development (OCID)
             </a><br></br>or email at <a className="text-[#2356f6]">ocid@carsu.edu.ph</a></p>
           </div>
-             {/*For part 2 footer*/}
-          <div className="flex flex-col md:flex-row items-center md:mx-16">
-            <img src='/images/CSU-Official-Seal_1216-x-2009-1.svg'
-            className="w-16 h-16 md:w-24 md:h-28"
-            ></img>
-            <img src='/images/Hi-Res-BAGONG-PILIPINAS-LOGO-1474x1536-1-1.svg'
-            className="w-16 h-16 md:w-24 md:h-28"
-            ></img>
-            <p className="text-center md:text-left mb-4 md:mb-0">
+          {/*For part 2 images*/}
+            <div className="flex flex-col md:flex-row items-center md:gap-4 md:mx-0 mx-4">
+              <div className="flex items-center md:gap-0 mx-28 md:mx-0">
+                  <img src='/images/CSU-Official-Seal_1216-x-2009-1.svg'
+                  className="flex flex-row w-16 h-16 md:w-24 md:h-28 gap-28"
+                  ></img>
+                  <img src='/images/Hi-Res-BAGONG-PILIPINAS-LOGO-1474x1536-1-1.svg'
+                  className="flex flex-row w-16 h-16 md:w-24 md:h-28 ml-auto md:ml-0"
+                  ></img>
+              </div>
+              <p className="text-center text-[9px] md:text-xs md:text-left  md:mb-0">
               Copyright &copy; Caraga State University {new Date().getFullYear()}
-            </p>
-            <div className="flex space-x-4 ml-auto md:mt-0">
+              </p>
+              <div className="text-[9px] md:text-xs text-center space-x-4 md:mt-0 md:ml-auto">
               <a href="#" className="text-white hover:underline transition-colors duration-200">
                 Privacy Policy
               </a>
@@ -34,6 +36,9 @@ const Footer = () => {
                 Contact
               </a>
             </div>
+            </div>
+          {/*For part 3 texts and links*/}
+          <div className="w-full flex flex-col md:flex-row md:items-center md:mx-16">
           </div>
         </div>
       </footer>
