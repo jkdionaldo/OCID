edb661e385d60426dfb42881de07c1599f8a704b
jan kevin <jankevin.dionaldo64@gmail.com>
2025-07-30 17:12:50 +0800

    fix: the homecard layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "../components/ui/Card";
+import Card from "../components/ui/HomeCard";
 // import Chatbot from "../components/chatbot/Chatbox";
 import BackToTop from "../components/ui/BackToTop";
 
