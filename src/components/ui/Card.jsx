92479ec8f111146e89001761de2c8aa79f5672bc
jan kevin <jankevin.dionaldo64@gmail.com>
2025-07-30 17:14:36 +0800

    fix: added the expord deault in the card

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -48,3 +48,5 @@ const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
 CardFooter.displayName = "CardFooter"
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+
+export default Card
