037cc324713a0583ef1d59ac866dbffa524c65f2
jan kevin <jankevin.dionaldo64@gmail.com>
2025-08-01 16:10:43 +0800

    remove the chatbox import

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
+import React from "react";
 import Card from "../components/ui/HomeCard";
+// import Chatbot from "../components/chatbot/Chatbox";
 
 const Home = () => {
   return (
-    <div className="flex flex-col w-full">
+    <div className="min-h-screen flex flex-col w-full">
       {/* Hero Section with Responsive OCID Banner */}
-      <div className="relative w-full">
+      <div className="container mx-auto relative max-w-fit">
         {/* Desktop Banner (hidden on small screens) */}
         <img
           src="/images/ocid-banner1.png"
@@ -43,7 +45,7 @@ const Home = () => {
       </div>
 
       {/* Content Sections*/}
-      <div className="py-6 sm:py-10 md:py-16 px-4 sm:px-8 md:px-16">
+      <div className="container mx-auto py-6 sm:py-10 md:py-16 px-4 sm:px-8 md:px-16">
         <div className="flex flex-col md:flex-row justify-center items-stretch gap-10 mb-8 sm:mb-12 md:mb-16">
           {/* Our Vision */}
           <Card>
@@ -129,7 +131,7 @@ const Home = () => {
         </div>
 
         {/*For Insitutional Outcomes*/}
-        <div className="flex flex-col md:flex-row gap-10 items-center md:items-start md:py-11 mb-12 ">
+        <div className="flex flex-col md:flex-row gap-10 items-center justify-center md:items-start max-w-8xl mx-auto md:py-11 mb-12 ">
           <img
             src="/images/institutional_outcome.svg"
             className="w-full max-w-md sm:max-w-md md:max-w-xl lg:max-w-2xl xl:max-w-2xl h-auto rounded-lg"
@@ -210,29 +212,31 @@ const Home = () => {
         </div>
       </div>
       {/*Educational Philosophy*/}
-      <div className="w-full bg-csuGreen py-12 px-4 md:px-16 flex flex-col md:flex-row items-center md:items-center justify-center">
-        {/* Left: Headings */}
-        <div className="flex-1 flex flex-col items-center md:items-start justify-center mb-8 md:mb-0 md:px-16">
-          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold font-poppins text-white text-center md:text-left leading-tight">
-            Educational
-          </h2>
-          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold font-poppins text-white text-center md:text-left leading-tight md:ml-24">
-            Philosophy
-          </h2>
-        </div>
-        {/* Right: Paragraph */}
-        <div className="flex-2 flex md:justify-start md:px-15">
-          <p className="text-justify md:text-justify text-xs md:text-base max-w-2xl mx-auto mt-0 text-white font-poppins">
-            Caraga State University upholds education as a transformative force
-            that creates futures and empowers communities. It is through
-            meaningful, values-driven learning that individuals are shaped into
-            competent professionals, ethical leaders, and innovative
-            problem-solvers. CSU believes that education should go beyond
-            knowledge transmission—it must inspire lifelong learning, cultivate
-            social responsibility, and enable graduates to lead change, foster
-            inclusivity, and contribute to the sustainable development of the
-            Caraga Region and beyond.
-          </p>
+      <div className="w-full bg-csuGreen py-12 px-4 md:px-16">
+        <div className="container mx-auto flex flex-col md:flex-row items-center justify-center gap-8">
+          {/* Left: Headings */}
+          <div className="flex-1 flex flex-col items-center md:items-start justify-center mb-8 md:mb-0 md:px-28">
+            <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold font-poppins text-white text-center md:text-left leading-tight">
+              Educational
+            </h2>
+            <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold font-poppins text-white text-center md:text-left leading-tight md:ml-24">
+              Philosophy
+            </h2>
+          </div>
+          {/* Right: Paragraph */}
+          <div className=" flex justify-center md:justify-start md:px-30">
+            <p className="text-justify md:text-justify text-xs md:text-base md:mr-16 max-w-3xl mx-auto md:mx-0 mt-0 text-white font-poppins">
+              Caraga State University upholds education as a transformative force
+              that creates futures and empowers communities. It is through
+              meaningful, values-driven learning that individuals are shaped into
+              competent professionals, ethical leaders, and innovative
+              problem-solvers. CSU believes that education should go beyond
+              knowledge transmission—it must inspire lifelong learning, cultivate
+              social responsibility, and enable graduates to lead change, foster
+              inclusivity, and contribute to the sustainable development of the
+              Caraga Region and beyond.
+            </p>
+          </div>
         </div>
       </div>
     </div>
